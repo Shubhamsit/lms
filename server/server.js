ec8e28616cf62eab940fddb1588a6b8e23f38fa0
Shubham Kumar <shubhamsit31@gmail.com>
2025-07-10 00:47:14 +0530

    ee

--- server/server.js
+++ server/server.js
@@ -38,7 +38,7 @@ app.get('/check',(req,res)=>{
 
 });
 
-app.post('/clerk', bodyParser.raw({ type: 'application/json' }), clerkWebhooks); ✅
+app.post('/clerk', bodyParser.raw({ type: 'application/json' }), clerkWebhooks); 
 
 // app.post('/clerk', express.json(), clerkWebhooks);
 
